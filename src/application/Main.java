7dc5c005c20a7b13ec5f04e922b042775f588b4a
Luiz Dorici <luizdorici@gmail.com>
2021-01-18 19:12:22 -0300

    metodo find all do seller implementado

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,12 +13,19 @@ public class Main {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		
-		System.out.println("===== TESTE 1 - Find by Id =====");
+		System.out.println("===== TESTE 1 - Seller Find by Id =====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		System.out.println("\n===== TESTE 2 - Find by Department =====");
+		
+		System.out.println("\n===== TESTE 2 - Seller Find by Department =====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller sell : list) {
+			System.out.println(sell);
+		}
+		
+		System.out.println("\n===== TESTE 3 - Seller Find All =====");
+		list = sellerDao.findAll();
 		for(Seller sell : list) {
 			System.out.println(sell);
 		}
