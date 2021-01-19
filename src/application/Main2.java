d9493d5f2907fd00a65a4f600d1023c50b916dff
Luiz Dorici <luizdorici@gmail.com>
2021-01-19 11:44:08 -0300

    Department find all order by id implementado

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -15,6 +16,12 @@ public class Main2 {
 		System.out.println("===== TESTE 1 - Department Find by Id =====");
 		Department department = departmentDao.findById(3);
 		System.out.println(department);
+		
+		System.out.println("\n===== TESTE 2 - Department Find All =====");
+		List<Department> list = departmentDao.findAll();
+		for(Department dep : list) {
+			System.out.println(dep);
+		}
 		
 		sc.close();
 	}
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,12 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
 import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
 	
@@ -22,13 +24,13 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 	}
 	
 	@Override
-	public void insert(Department obj) {
+	public void insert(Department dep) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void update(Department obj) {
+	public void update(Department dep) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -76,8 +78,30 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT department.* "
+					+ "FROM department "
+					+ "ORDER BY Id");
+			
+			rs = st.executeQuery();
+			List<Department> list = new ArrayList<>();
+			
+			while(rs.next()) {
+				Department dp = instantiateDepartment(rs);
+				list.add(dp);
+			}
+			return list;
+			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
-
 }
